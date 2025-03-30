093b9fdb166ac8b1c7ca98e735ea79f45cca66fc
徐博 洪 <2012753288@qq.com>
2025-03-30 16:34:58 +0800

    feat: 初始化项目结构

---
+++ backend/CodePilot_Backend/src/main/java/com/deepmind/codepilot_backend/CodePilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deepmind.codepilot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePilotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePilotBackendApplication.class, args);
+    }
+
+}
---
+++ backend/CodePilot_Backend/src/test/java/com/deepmind/codepilot_backend/CodePilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepmind.codepilot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePilotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
