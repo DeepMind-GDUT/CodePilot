6a5e5c9f21541580bf2b97b837dc69b2d6664ec8
徐博 洪 <2012753288@qq.com>
2025-05-19 11:04:22 +0800

    feat: 新增社区模块初始化功能

--- backend/CodePilot_Backend/src/main/java/com/deepmind/controller/CommunityController.java
+++ backend/CodePilot_Backend/src/main/java/com/deepmind/controller/CommunityController.java
@@ -1,4 +1,16 @@
 package com.deepmind.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController("/Community")
 public class CommunityController {
+    @GetMapping("/init")
+    public List<String> CommunityInit(){
+        return new ArrayList<>();
+    }
 }
